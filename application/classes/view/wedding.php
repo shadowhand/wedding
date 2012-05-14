a74ec4778b673856fcb82d787b5ace68930ebb1a
Woody Gilk <woody.gilk@gmail.com>
2012-05-13 19:39:57 -0500

    Installed shadowhand/email to modules/email

--- application/classes/view/wedding.php
+++ application/classes/view/wedding.php
@@ -4,5 +4,10 @@ abstract class View_Wedding extends Kostache_Layout {
 
 	protected $_layout = 'layout';
 
+	public function charset()
+	{
+		return Kohana::$charset;
+	}
+
 }
 
--- application/classes/view/wedding/home.php
+++ application/classes/view/wedding/home.php
@@ -2,5 +2,45 @@
 
 class View_Wedding_Home extends View_Wedding {
 
-}
+    public $your_name;
+    public $total_guests;
+    public $phone_numer;
 
+    protected $_places = array(
+            array(
+                'name' => 'Naniboujou Lodge',
+                'comment' => 'Half way between Grand Marais and Hovland, a beautiful, quiet spot by the lake.',
+                'website' => 'http://www.naniboujou.com/',
+                'address' => '20 Naniboujou Trail, Grand Marais, MN 55604',
+                'telephone' => '218 387-2688',
+            ),
+            array(
+                'name' => 'Easy Bay Suites',
+                'comment' => 'Downtown suites on the beach, next door to guided kayaking and the food co-op.',
+                'website' => 'http://www.eastbaysuites.com/',
+                'address' => '21 Wisconsin Street, Grand Marais, MN 55604',
+                'telephone' => '800 414-2807',
+            ),
+            array(
+                'name' => 'Gunflint Motel & Cabins',
+                'comment' => 'Small inn with suites very suitable for a family.',
+                'website' => 'http://www.gunflintmotel.com/',
+                'address' => '101 5th Ave W, Grand Marais, MN 55604',
+                'telephone' => '218 387-1454',
+            ),
+        );
+    
+    public function lodgings()
+    {
+        $lodgings = array();
+
+        foreach ($this->_places as $place)
+        {
+            $place['map'] = http_build_query(array('q' => $place['address']));
+            $lodgings[] = $place;
+        }
+
+        return $lodgings;
+    }
+
+}
